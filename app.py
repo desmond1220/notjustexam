1f45d2e4eebf1c2ce2cf675141c3c5f1f5a2985c
Desmond <desmond971220@gmail.com>
2025-10-26 15:34:37 +0800

    fix div

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ body{{padding:4px}}
     if ai_html:
         html += f'<div class="answer-content"><h5>🤖 AI Recommendation</h5><div style="padding:10px">{ai_html}</div></div>'
 
-    html += '</div>\n</div>\n'
+    # html += '</div>\n</div>\n'
 
     # Add JavaScript
     html += f'''
